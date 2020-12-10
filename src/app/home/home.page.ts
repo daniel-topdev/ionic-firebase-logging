0774e25cdb3259ebe1f8bd941bd9757b9ee0e76c
daniel-topdev <danielsantosdev747@gmail.com>
2020-12-10 10:21:03 -0500

    updates UI

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { AnalyticsService } from '../services/analytics.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  enabled = this.analyticsService.analyticsEnabled;
+
+  constructor(private analyticsService: AnalyticsService) { }
+
+  setUser() {
+    this.analyticsService.setUser();
+  }
+
+  setProperty() {
+    this.analyticsService.setProperty();
+  }
+
+  logEvent() {
+    this.analyticsService.logEvent();
+  }
+  
+  toggleDataCollection() {
+    this.analyticsService.toggleAnalytics();
+    this.enabled = this.analyticsService.analyticsEnabled;
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    //here is firebase configuration info
+  }
+};
